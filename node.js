22f0f7190f5420da1ae0cca9bd66ca4d0a70db75
gautam <gkachru@gmail.com>
2021-06-14 21:16:02 +0530

    Removed WRTC hard requirement

--- node.js
+++ node.js
@@ -1,5 +1,4 @@
 const BrowserP2PT = require('./p2pt')
-const wrtc = require('wrtc')
 
 class P2PT extends BrowserP2PT {
   /**
@@ -7,7 +6,7 @@ class P2PT extends BrowserP2PT {
    * @param array announceURLs List of announce tracker URLs
    * @param string identifierString Identifier used to discover peers in the network
    */
-  constructor (announceURLs = [], identifierString = '') {
+  constructor (wrtc, announceURLs = [], identifierString = '') {
     super(announceURLs, identifierString)
 
     this._wrtc = wrtc
